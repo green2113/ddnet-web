b4c6c9b59ca638ed17f7337fb26b2e2dbf86efa1
green2113 <kangmw081122@gmail.com>
2025-08-08 15:30:37 +0900

    web: deploy updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:4000',
-      '/auth': 'http://localhost:4000',
-      '/socket.io': {
-        target: 'ws://localhost:4000',
-        ws: true,
-      },
-    },
-  },
 })
