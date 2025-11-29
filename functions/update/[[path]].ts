34bfa11eb45e37f3512625cae5aed0323dd13f01
green2113 <kangmw081122@gmail.com>
2025-11-30 01:07:04 +0900

    feat(web): onRequest 함수에서 .exe 파일에 대한 리다이렉트 로직 추가 및 정적 파일 처리 로직 수정

--- functions/update/[[path]].ts
+++ functions/update/[[path]].ts
@@ -7,13 +7,11 @@ export async function onRequest(context: {
   const segments = params.path ?? [];
   const path = segments.join("/");
 
-  // 정적 파일로 서빙해야 하는 경우
-  if (path === "update.json" || path === "info.json") {
-    return env.ASSETS.fetch(request);
+  if (path.endsWith(".exe")) {
+    const releaseUrl =
+      "https://github.com/<user>/<repo>/releases/download/<tag>/" + path;
+    return Response.redirect(releaseUrl, 302);
   }
 
-  // 나머지는 GitHub Releases로 리다이렉트
-  const releaseUrl =
-    "https://github.com/green2113/ddnet-web/releases/download/client/" + path;
-  return Response.redirect(releaseUrl, 302);
+  return env.ASSETS.fetch(request);
 }
