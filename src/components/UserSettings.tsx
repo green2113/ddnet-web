269f621a0b619fb39cc157d9f6e52d3ab6b5b466
green2113 <76150415+green2113@users.noreply.github.com>
2025-12-29 18:19:06 +0900

    Update UserSettings.tsx

--- src/components/UserSettings.tsx
+++ src/components/UserSettings.tsx
@@ -47,7 +47,7 @@ export default function UserSettings({
 
   return createPortal(
     <div
-      className="fixed inset-0 z-[80] flex items-center justify-center px-8 py-5"
+      className="fixed inset-0 z-[80] flex items-center justify-center px-10 py-7"
       style={{ background: 'rgba(0,0,0,0.65)' }}
       onMouseDown={onCloseUserSettings}
     >
