cc10ef8cd688304aa8c5d48dc7c4345858aafb60
green2113 <kangmw081122@gmail.com>
2026-01-04 12:57:55 +0900

    feat: Vite 설정에서 환경 변수 로드 및 Electron 지원 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const isElectron = env.VITE_APP_TARGET === 'electron'
+  return {
+    plugins: [react()],
+    base: isElectron ? './' : '/',
+  }
 })
